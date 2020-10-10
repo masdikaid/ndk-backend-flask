b9c9215cf5a32d9273efea41a8e20759e4de4c74
masdika ID <masdikaid@gmail.com>
2020-10-10 20:19:55 +0700

    build route

--- redirector/route.py
+++ redirector/route.py
@@ -1,13 +1,20 @@
-from flask import redirect
-from .redirector import redirectapp
+from flask import redirect, render_template
+from .redirector import redirectapp, Url
 
 @redirectapp.route("/")
 def index():
-    return redirect("http://cendek.com")
+    return "<h1>Hello</h1>"
 
 @redirectapp.route("/<string:urlid>")
 def redirector(urlid):
-    pass
+    # try :
+    urldata = Url.get(urlid)
+    if len(urldata.target_url) > 1:
+        return urldata.target_url
+    else :
+        return redirect(urldata.target_url[0])
+    # except ValueError:
+    #     return redirect("404")
 
 
     
